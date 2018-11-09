f9cce4309c6b6c85eab46b379b56ce35540fa7b3
Alex <a.mas.bosch@gmail.com>
2018-11-09 16:27:47 +0100

    delegated event handling flow to middleware via next

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ export interface ThreadManagerConfig {
 
 const defaultConfiguration = {
     distributionStrategy: WorkDistributionStrategy.ROUND_ROBIN,
-    amountOfWorkers: window.navigator.hardwareConcurrency/2,
+    amountOfWorkers: window.navigator.hardwareConcurrency / 2,
     initializationStrategy: InitializationStrategy.AT_START,
     sendingStrategy: MessageSendingStrategy.DEFAULT
 }
@@ -87,7 +87,7 @@ export class ThreadManager {
         if (onError && typeof onError === 'function') {
             this.onError = onError;
         }
-        if(this.config.initializationStrategy === InitializationStrategy.AT_START){
+        if (this.config.initializationStrategy === InitializationStrategy.AT_START) {
             this.initializeWorkers(this.config.amountOfWorkers);
         }
     }
@@ -108,28 +108,68 @@ export class ThreadManager {
             const worker = this.workers[index];
             worker.id = index;
             worker.status = WorkerStatus.IDLE;
-            worker.onmessage= this.eventHandler;
+            worker.onmessage = this.eventHandler;
             return worker;
         } else {
             console.warn('Adding more threads that exceed the configured ammount, change the configured ammount instead');
         }
     }
-    eventHandler = (event: MessageEvent)=>{
-        //TODO: abstract middleware logic into a separate function so that it can be used for errors too
-        //TODO: Delegate control flow to middlewares so they can stop event propagation by not calling next handler
-       //this.status  = WorkerStatus.IDLE;
-       for (let i = 0; i < this.middleware.length; i++) {
-            this.middleware[i](event);
+    //TODO: Return current handler diferently in function of the type of event
+    // if error, all middleware will be iterated and as last onError will be called
+    //else middleware will be iterated and onMessage will be called
+    _wip_messageHandler = (event: MessageEvent | ErrorEvent)=>{
+        const next = () => {
+            const currentHandler = middleware.next();
+            if(!currentHandler.done){
+                currentHandler.value(event, next);
+            }
         }
-        if(this.onMessage){
-            this.onMessage(event);
+        const getCurrentHandler = (index: number)=>{
+            return this.middleware[index] || this.onMessage;
         }
-      
+
+        const middleware = (function* () {
+            let index = 0;
+            let func = getCurrentHandler(index);
+            while(func){
+                yield func;
+                index++;
+                func = getCurrentHandler(index);
+            }
+     
+        })();
+
     }
-    errorHandler = (event: ErrorEvent)=>{
+    eventHandler = (event: MessageEvent) => {
+
+        const next = () => {
+            const currentHandler = middleware.next();
+            if(!currentHandler.done){
+                currentHandler.value(event, next);
+            }
+        }
+        const getCurrentHandler = (index: number)=>{
+            return this.middleware[index] || this.onMessage;
+        }
+
+        const middleware = (function* () {
+            let index = 0;
+            let func = getCurrentHandler(index);
+            while(func){
+                yield func;
+                index++;
+                func = getCurrentHandler(index);
+            }
+     
+        })();
+        //this.status  = WorkerStatus.IDLE;
+        next();
+
+    }
+    errorHandler = (event: ErrorEvent) => {
         //TODO: allow middleware to handle errors too
         //this.status = WorkerStatus.CRASHED;
-        if(this.onError){
+        if (this.onError) {
             this.onError(event);
         }
     };
@@ -140,7 +180,7 @@ export class ThreadManager {
         this.onMessage = eHandler;
     };
 
-    setErrorHandler = (eHandler: ErrorEventHandler)=>{
+    setErrorHandler = (eHandler: ErrorEventHandler) => {
         if (!eHandler || typeof eHandler !== 'function') {
             throw new Error('Expected a function as argument and got a ' + typeof eHandler);
         }
@@ -171,7 +211,7 @@ export class ThreadManager {
     }
 
 
-    broadcastMessage = (type: string, payload: any)=> {
+    broadcastMessage = (type: string, payload: any) => {
         if (this.workers.length < this.config.amountOfWorkers) {
             this.initializeWorkers(this.config.amountOfWorkers - this.workers.length);
         }
@@ -179,7 +219,7 @@ export class ThreadManager {
             this.giveWork(this.workers[i], type, payload);
         }
     }
-    
+
 
     giveWork = (worker: EnhancedWorker, type: string, payload: any) => {
         let data = { type, payload };
@@ -187,7 +227,7 @@ export class ThreadManager {
             //TODO: keep same laoyut as other sending methods
             worker.postMessage(payload, [payload]);
         } else if (this.config.sendingStrategy === MessageSendingStrategy.JSON) {
-            
+
             worker.postMessage(JSON.stringify(data));
         } else {
             worker.postMessage(data);
@@ -237,7 +277,7 @@ export class ThreadManager {
     }
     createAndGiveWork = (event: string, payload: any) => {
         const newWorker = this.initializeWorker();
-        if(newWorker){
+        if (newWorker) {
             this.giveWork(newWorker, event, payload);
         }
     };
